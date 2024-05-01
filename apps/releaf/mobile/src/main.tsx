06412612438e6de6a6943148f0a08c930f06b543
Andrew Parker <andrew.parker@genparker.com>
2024-05-01 19:30:01 -0400

    first inttegration of mobile support

--- apps/releaf/mobile/src/main.tsx
+++ apps/releaf/mobile/src/main.tsx
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './app/App';
-import './app.css';
+// import './app.css';
 
 AppRegistry.registerComponent('ReleafMobileApp', () => App);
