993689c08a1ceeb22b6f1d8502913f74f21a936f
Andrew Parker <andrew.parker@genparker.com>
2024-08-07 22:12:31 -0400

    adding qr code generator in the project! :D

--- apps/releaf/qr-code/src/app/NxWelcome.vue
+++ apps/releaf/qr-code/src/app/NxWelcome.vue
@@ -1,13 +1,23 @@
-<script setup lang="ts">
-defineProps<{
-  title: string;
-}>();
-</script>
-
 <template>
   <h1>
     <span> Hello there, 3 </span> Welcome {{ title }} 👋
   </h1>
+  <qrcode-vue :value="value" :level="level" :render-as="renderAs" />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import QrcodeVue from 'qrcode.vue'
+import type { Level, RenderAs } from 'qrcode.vue'
+
+defineProps<{
+  title: string;
+}>();
+
+const value = 'hey hey heeeeeeeeeeeeey, hey hey heeeeeeeeeeeeey, hey hey heeeeeeeeeeeeey'
+const level = ref<Level>('L')
+const renderAs = ref<RenderAs>('svg')
+
+</script>
 
 <style scoped></style>
