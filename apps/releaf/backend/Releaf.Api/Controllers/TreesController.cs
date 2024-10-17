b3e00786439c4a64babebb26175255f7d26a29bd
Andrew Parker <andrew.parker@beslogic.com>
2024-10-16 21:00:04 -0400

    prod we go!!!

--- apps/releaf/backend/Releaf.Api/Controllers/TreesController.cs
+++ apps/releaf/backend/Releaf.Api/Controllers/TreesController.cs
@@ -18,10 +18,9 @@ public class TreesController : ControllerBase
   }
 
   [HttpGet]
-  public IDictionary<string, string> GetAll(int page, int size)
+  public IEnumerable<TreeDefinitionAggregate> GetAll(int page, int size)
   {
-    IEnumerable<TreeDefinitionAggregate> trees = treeRepo.GetAll(page, size);
-    return trees.ToDictionary(t => t.Id.Value, t => t.Name);
+    return treeRepo.GetAll(page, size);
   }
 
   [HttpGet("{id}")]
